0e456bc72a4cf9cbed0309d2dff0573fc7ba8cc9
Zergity <zergity@gmail.com>
2021-01-16 15:07:08 +0700

    add web3-react connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useEagerConnect, useInactiveListener } from './hooks'
+import { Spinner } from './components/Spinner'
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  injected,
+} from './connectors'
+
+function getErrorMessage(error) {
+  if (error instanceof NoEthereumProviderError) {
+    return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network."
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected
+    // || error instanceof UserRejectedRequestErrorWalletConnect
+    // || error instanceof UserRejectedRequestErrorFrame
+  ) {
+    return 'Please authorize this website to access your Ethereum account.'
+  } else {
+    console.error(error)
+    return 'An unknown error occurred. Check the console for more details.'
+  }
 }
 
-export default App;
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+const connectorsByName = {
+  ['Injected']: injected,
+}
+
+function App () {
+
+  const context = useWeb3React()
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context
+
+  // handle logic to recognize the connector currently being activated
+  const [activatingConnector, setActivatingConnector] = React.useState()
+  React.useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined)
+    }
+  }, [activatingConnector, connector])
+
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
+  const triedEager = useEagerConnect()
+
+  // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+  useInactiveListener(!triedEager || !!activatingConnector)
+
+  return (
+      <div className="App">
+        <div>
+          {Object.keys(connectorsByName).map(name => {
+            const currentConnector = connectorsByName[name]
+            const activating = currentConnector === activatingConnector
+            const connected = currentConnector === connector
+            const disabled = !triedEager || !!activatingConnector || connected || !!error
+
+            return (
+              <button
+                disabled={disabled}
+                key={name}
+                onClick={() => {
+                  setActivatingConnector(currentConnector)
+                  activate(connectorsByName[name])
+                }}
+              >
+                {connected ? <span role="img" aria-label="check">✅</span> :
+                  activating ? <Spinner color={'black'}/> :
+                    name
+                }
+              </button>
+            )
+          })}
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {(active || error) && (
+            <button
+              onClick={() => {
+                deactivate()
+              }}
+            >
+              Deactivate
+            </button>
+          )}
+
+          {!!error && <h4 style={{ marginTop: '1rem', marginBottom: '0' }}>{getErrorMessage(error)}</h4>}
+        </div>
+
+      </div>
+  )
+}
+
+export default function() {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  )
+}
