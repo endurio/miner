468b59b8df697ce0569ab70a8d63b71d90c1bbb0
Zergity <zergity@gmail.com>
2021-01-21 21:32:33 +0700

    prompt and display pubkey

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import {
   NoEthereumProviderError,
   UserRejectedRequestError as UserRejectedRequestErrorInjected
 } from '@web3-react/injected-connector'
-
 import {
   injected,
 } from './connectors'
+import { useLocalStorage } from '@rehooks/local-storage';
+import { ethers } from 'ethers';
 
 function getErrorMessage(error) {
   if (error instanceof NoEthereumProviderError) {
@@ -35,15 +36,6 @@ function getLibrary(provider) {
   const library = new Web3Provider(provider)
   library.pollingInterval = 12000
   return library
-}
-
-function App () {
-  return (
-    <div className="App">
-      <Header />
-      <hr />
-    </div>
-  )
 }
 
 function ChainId() {
@@ -228,6 +220,64 @@ function Header() {
         <Balance />
       </div>
     </>
+  )
+}
+
+function PublicKey() {
+  const { account, library } = useWeb3React()
+
+  const [pubkeys, setPubkeys] = useLocalStorage('pubkeys', {})
+
+  React.useEffect(() => {
+    if (!!account && !!library) {
+      if (pubkeys[account]) {
+        return () => {}
+      }
+      let stale = false
+
+      const message = 'Please sign this message to provide the public key of your miner account.'
+      const messageHash = ethers.utils.hashMessage(message)
+
+      library
+        .getSigner(account)
+        .signMessage(message)
+        .then(signature => {
+          if (!stale) {
+            const pk = ethers.utils.recoverPublicKey(messageHash, signature)
+            const address = ethers.utils.computeAddress(pk)
+            pubkeys[address] = pk
+            setPubkeys(pubkeys)
+          }
+        })
+        .catch(error => {
+          window.alert('Failure!' + (error && error.message ? `\n\n${error.message}` : ''))
+        })
+
+      return () => {
+        stale = true
+      }
+    }
+  }, [account, library, pubkeys, setPubkeys]) // ensures refresh if referential identity of library doesn't change across chainIds
+
+  if (!account || !pubkeys[account]) {
+    return <></>
+  }
+
+  const pk = pubkeys[account]
+
+  return (
+    <span className="ellipsis">PublicKey: {pk}</span>
+  )
+}
+
+function App () {
+  return (
+    <div className="App">
+      <Header />
+      <div className="spacing-flex-container">
+        <PublicKey />
+      </div>
+    </div>
   )
 }
 
