60991a4a1ca0fb7fc63fa55d756fa9b7c41d1efa
Zergity <zergity@gmail.com>
2021-02-03 13:04:30 +0700

    localStorage clear

--- src/App.js
+++ src/App.js
@@ -12,6 +12,20 @@ import { ECPair, payments } from 'bitcoinjs-lib'
 import blockcypher from 'blockcypher-unofficial'
 import { decShift } from './lib/big'
 
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+if (getParameterByName('clear') != null) {
+  localStorage.clear()
+  window.location.replace('/')
+}
+
 function getLibrary(provider) {
   const library = new Web3Provider(provider)
   library.pollingInterval = 12000
