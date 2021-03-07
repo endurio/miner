6793ae2b6bbbc2f1336efa991ae3159bce852700
Zergity <zergity@gmail.com>
2021-03-07 21:10:27 +0700

    support cordova and background mode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+  const cordova = window.cordova
+  // Cordova is now initialized. Have fun!
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+  cordova.plugins.backgroundMode.enable()
+  document.getElementById('deviceready').classList.add('ready');
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
